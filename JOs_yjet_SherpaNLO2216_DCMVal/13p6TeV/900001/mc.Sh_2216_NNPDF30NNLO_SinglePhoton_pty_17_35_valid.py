c139d1a056facc8cf65cb8528df5724ce71f5f1b
dcamarer <daniel.camarero.munoz@cern.ch>
2026-01-26 15:28:36 +0100

    Separate folder structure for Run 2 and Run 3 (III)

---
+++ JOs_yjet_SherpaNLO2216_DCMVal/13p6TeV/900001/mc.Sh_2216_NNPDF30NNLO_SinglePhoton_pty_17_35_valid.py
@@ -0,0 +1,57 @@
+include("Sherpa_i/Base_Fragment.py")
+include("Sherpa_i/NNPDF30NNLO.py")
+
+evgenConfig.description = "Sherpa gamma + 1,2j@NLO + 3,4j@LO with 17<pT_y<35."
+evgenConfig.keywords = [ "SM", "photon", "jets", "NLO" ]
+evgenConfig.contact  = [ "atlas-generators-sherpa@cern.ch", "daniel.camarero.munoz@cern.ch" ]
+evgenConfig.nEventsPerJob = 1000
+
+genSeq.Sherpa_i.RunCard="""
+(run){
+  # Perturbative scales
+  FSF:=1.; RSF:=1.; QSF:=1.;
+  SCALES STRICT_METS{FSF*MU_F2}{RSF*MU_R2}{QSF*MU_Q2};
+  CORE_SCALE VAR{PPerp2(p[2])};
+  ALPHAQED_DEFAULT_SCALE=0.0;
+  
+  # ME generator settings
+  ME_SIGNAL_GENERATOR Comix Amegic LOOPGEN;
+  LOOPGEN:=OpenLoops;
+  
+  # Tags for process setup
+  NJET:=4; LJET:=2,3; QCUT:=20.;
+
+  # EW corrections setup
+  ASSOCIATED_CONTRIBUTIONS_VARIATIONS=EW EW|LO1 EW|LO1|LO2;
+  EW|LO1|LO2|LO3;
+  METS_BBAR_MODE=5;
+  EW_SCHEME=3;
+  GF=1.166397e-5;
+  KFACTOR VAR{128.802/137.03599976};
+}(run)
+
+(processes){
+  Process 93 93 -> 22 93 93{NJET};
+  Order (*,1);
+  CKKW sqr(QCUT/E_CMS)/(1.0+sqr(QCUT/0.6)/PPerp2(p[2]));
+  Associated_Contributions EW|LO1|LO2|LO3 {LJET};
+  NLO_QCD_Mode MC@NLO {LJET};
+  ME_Generator Amegic {LJET};
+  RS_ME_Generator Comix {LJET};
+  Loop_Generator LOOPGEN {LJET};
+  Integration_Error 0.10 {2,3,4,5,6,7,8};
+  PSI_ItMin 30000 {2,3}
+  PSI_ItMin 50000 {4,5,6}
+  End process;
+}(processes)
+
+(selector){
+  IsolationCut  22  0.1  2  0.10
+  PTNLO 22  17  35
+  RapidityNLO  22  -2.7  2.7
+}(selector)
+"""
+
+genSeq.Sherpa_i.NCores = 256
+genSeq.Sherpa_i.Parameters += [ "OL_PARAMETERS=write_parameters=1" ]
+genSeq.Sherpa_i.Parameters += [ "EW_SCHEME=3", "GF=1.166397e-5" ]
