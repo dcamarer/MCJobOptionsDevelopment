cc17e97486db5d1eb4c02e96eb4784c7b2c67558
dcamarer <daniel.camarero.munoz@cern.ch>
2026-02-05 19:57:16 +0100

    Preparing condor scripts for the modified ssWW production.

---
+++ JOs_ssww_Sherpa303_DCMVal/13p6TeV/gen_999997.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+asetup AthGeneration,23.6.61,here
+
+WORKDIR="/usatlas/u/dcamarero/MCJobOptionsDevelopment/JOs_ssww_Sherpa303_DCMVal/13p6TeV/999997_gen" # Ensure working dir
+
+# Check if workdir exists, abort if not
+if [ ! -d "$WORKDIR" ]; then
+    echo "ERROR: Workdir does not exist: $WORKDIR" >&2
+    echo "Aborting execution." >&2
+    exit 1
+fi
+
+cd "$WORKDIR" || exit 1
+
+Gen_tf.py --ecmEnergy=13600.0 \
+          --randomSeed=1234 \
+          --jobConfig=../999997 \
+          --outputEVNTFile=run2_999997.EVNT.root \
+          --maxEvents=1000
