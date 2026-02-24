c0e77ec61befeace12e404bb9c3665a15a1b6a3e
dcamarer <daniel.camarero.munoz@cern.ch>
2026-02-25 00:10:10 +0100

    Fixing the relative paths for ssWW polarisation scripts (III)

--- JOs_ssww_Sherpa303_DCMVal/13p6TeV/gen_701380.sh
+++ JOs_ssww_Sherpa303_DCMVal/13p6TeV/gen_701380.sh
@@ -4,7 +4,7 @@ export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 asetup AthGeneration,23.6.61,here
 
-WORKDIR="/afs/cern.ch/work/d/dcamarer/private/PostDoc/PMG/MCJobOptionsDevelopment/JOs_ssww_Sherpa303_DCMVal/13TeV/701380_gen" # Ensure working dir
+WORKDIR="/afs/cern.ch/work/d/dcamarer/private/PostDoc/PMG/MCJobOptionsDevelopment/JOs_ssww_Sherpa303_DCMVal/13p6TeV/701380_gen" # Ensure working dir
 
 # Check if workdir exists, abort if not
 if [ ! -d "$WORKDIR" ]; then
