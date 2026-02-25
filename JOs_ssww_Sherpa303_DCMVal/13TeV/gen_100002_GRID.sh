4a519bbea273ce1781a5f622fc9963255461c8fc
dcamarer <daniel.camarero.munoz@cern.ch>
2026-02-25 18:11:52 +0100

    Creating some grid scripts for ssWW event generation (II).

--- JOs_ssww_Sherpa303_DCMVal/13TeV/gen_100002_GRID.sh
+++ JOs_ssww_Sherpa303_DCMVal/13TeV/gen_100002_GRID.sh
@@ -2,7 +2,7 @@ echo ""
 date
 echo ""
 
-pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:1234 --outputEVNTFile %OUT.EVNT.root --jobConfig=100002 --maxEvents=30000" --outDS user.dcamarer.run2_100002 --split 6 --maxCpuCount 259200
+pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:1234 --outputEVNTFile %OUT.EVNT.root --jobConfig=100002 --maxEvents=30000" --outDS user.dcamarer.run2_100002 --split 10 --maxCpuCount 259200
 
 # --nFilesPerJob 1
 
--- JOs_ssww_Sherpa303_DCMVal/13p6TeV/gen_100000_GRID.sh
+++ JOs_ssww_Sherpa303_DCMVal/13p6TeV/gen_100000_GRID.sh
@@ -2,7 +2,7 @@ echo ""
 date
 echo ""
 
-pathena --trf "Gen_tf.py --ecmEnergy=13600 --randomSeed %RNDM:1234 --outputEVNTFile %OUT.EVNT.root --jobConfig=100000 --maxEvents=30000" --outDS user.dcamarer.run2_100000 --split 6 --maxCpuCount 259200
+pathena --trf "Gen_tf.py --ecmEnergy=13600 --randomSeed %RNDM:1234 --outputEVNTFile %OUT.EVNT.root --jobConfig=100000 --maxEvents=30000" --outDS user.dcamarer.run3_100000 --split 10 --maxCpuCount 259200
 
 # --nFilesPerJob 1
 
