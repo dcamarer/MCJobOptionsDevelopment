682bfa36f5941715c45b1692d353d01d1e8aa46f
dcamarer <daniel.camarero.munoz@cern.ch>
2026-03-19 00:16:34 +0100

    Doing another extension for ssWW Run 3

---
+++ JOs_ssww_Sherpa303_DCMVal/13p6TeV/gen_100000_part9.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+asetup AthGeneration,23.6.60,here
+
+WORKDIR="/afs/cern.ch/work/d/dcamarer/private/PostDoc/PMG/MCJobOptionsDevelopment/JOs_ssww_Sherpa303_DCMVal/13p6TeV/100000_gen_part9" # Ensure working dir
+
+# Check if workdir exists, abort if not
+if [ ! -d "$WORKDIR" ]; then
+    echo "ERROR: Workdir does not exist: $WORKDIR" >&2
+    echo "Aborting execution." >&2
+    exit 1
+fi
+
+cd "$WORKDIR" || exit 1
+
+Gen_tf.py --ecmEnergy=13600.0 \
+          --randomSeed=1222 \
+          --jobConfig=../100000 \
+          --outputEVNTFile=run3_100000_part9.EVNT.root \
+          --maxEvents=5000
---
+++ JOs_ssww_Sherpa303_DCMVal/13p6TeV/gen_100002_part18.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+asetup AthGeneration,23.6.60,here
+
+WORKDIR="/afs/cern.ch/work/d/dcamarer/private/PostDoc/PMG/MCJobOptionsDevelopment/JOs_ssww_Sherpa303_DCMVal/13p6TeV/100002_gen_part18" # Ensure working dir
+
+# Check if workdir exists, abort if not
+if [ ! -d "$WORKDIR" ]; then
+    echo "ERROR: Workdir does not exist: $WORKDIR" >&2
+    echo "Aborting execution." >&2
+    exit 1
+fi
+
+cd "$WORKDIR" || exit 1
+
+Gen_tf.py --ecmEnergy=13600.0 \
+          --randomSeed=4041 \
+          --jobConfig=../100002 \
+          --outputEVNTFile=run3_100002_part18.EVNT.root \
+          --maxEvents=500
