e576be794a79641086bc9fe64fbf13537d9535fc
dcamarer <daniel.camarero.munoz@cern.ch>
2026-03-19 11:55:22 +0100

    Updating the GRID scripts for Sherpa NLO (II).

--- JOs_yjet_SherpaNLO2216_DCMVal/13TeV/900004_gen_GRID/gen_900004_GRID.sh
+++ JOs_yjet_SherpaNLO2216_DCMVal/13TeV/900004_gen_GRID/gen_900004_GRID.sh
@@ -2,7 +2,10 @@ echo ""
 date
 echo ""
 
-pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:1234 --outputEVNTFile %OUT.EVNT.root --jobConfig=900004 --maxEvents=10000" --outDS user.dcamarer.run2_900004_260319 --split 10 --maxCpuCount 259200
+# Needed to unpack and avoid libraries issues (e.g. Have to compile Amegic libraries, you probably want to run ./makelibs)
+Gen_tf.py --ecmEnergy=13000 --randomSeed=9999 --jobConfig=900004 --outputEVNTFile=test.EVNT.root --maxEvents=1
+
+pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:9999 --outputEVNTFile %OUT.EVNT.root --jobConfig=900004 --maxEvents=10000" --outDS user.dcamarer.run2_900004_260319 --split 10 --maxCpuCount 259200
 
 # --nFilesPerJob 1
 
