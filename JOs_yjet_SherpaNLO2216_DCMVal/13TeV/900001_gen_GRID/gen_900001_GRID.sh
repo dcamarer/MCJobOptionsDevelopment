95d3912047f0d762ba9a5ab837ae69522e039774
dcamarer <daniel.camarero.munoz@cern.ch>
2026-03-20 07:41:22 +0100

    Alternative approach for GRID SNLO 2216 (II).

--- JOs_yjet_SherpaNLO2216_DCMVal/13TeV/900001_gen_GRID/gen_900001_GRID.sh
+++ JOs_yjet_SherpaNLO2216_DCMVal/13TeV/900001_gen_GRID/gen_900001_GRID.sh
@@ -2,10 +2,21 @@ echo ""
 date
 echo ""
 
-cd 900001
+# Needed to unpack and avoid libraries issues (e.g. Have to compile Amegic libraries, you probably want to run ./makelibs)
+Gen_tf.py --ecmEnergy=13000 --randomSeed=9999 --jobConfig=900001 --outputEVNTFile=test.EVNT.root --maxEvents=1
+
+echo ""
+echo " ===> DCM : Local generation done"
+
 ./makelibs
-cd ../
+
+echo ""
+echo " ===> DCM : ./makelibs done"
+
 pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:9999 --outputEVNTFile %OUT.EVNT.root --jobConfig=900001 --maxEvents=15000" --outDS user.dcamarer.run2_900001_260319 --split 10 --maxCpuCount 259200
+
+echo ""
+echo " ===> DCM : Grid generation job submission"
 
 # --nFilesPerJob 1
 
