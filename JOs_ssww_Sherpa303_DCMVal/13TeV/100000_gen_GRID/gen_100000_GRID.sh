b611cc4ce77cca115542ead421ad83f2b0c206e3
dcamarer <daniel.camarero.munoz@cern.ch>
2026-03-21 13:30:12 +0100

    Updating the name of the output.

--- JOs_ssww_Sherpa303_DCMVal/13TeV/100000_gen_GRID/gen_100000_GRID.sh
+++ JOs_ssww_Sherpa303_DCMVal/13TeV/100000_gen_GRID/gen_100000_GRID.sh
@@ -3,7 +3,7 @@ date
 echo ""
 
 #pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:9999 --outputEVNTFile %OUT.EVNT.root --jobConfig=100000 --maxEvents=5000" --outDS user.dcamarer.run2_100000_260319 --split 10 --maxCpuCount 259200
-pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:9998 --outputEVNTFile %OUT.EVNT.root --jobConfig=100000 --maxEvents=10000" --outDS user.dcamarer.run2_100000_260321 --split 10 --maxCpuCount 259200
+pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:9998 --outputEVNTFile %OUT.EVNT.root --jobConfig=100000 --maxEvents=10000" --outDS user.dcamarer.run2_100000_260321_bis --split 10 --maxCpuCount 259200
 
 echo ""
 date
--- JOs_ssww_Sherpa303_DCMVal/13TeV/100002_gen_GRID/gen_100002_GRID.sh
+++ JOs_ssww_Sherpa303_DCMVal/13TeV/100002_gen_GRID/gen_100002_GRID.sh
@@ -3,7 +3,7 @@ date
 echo ""
 
 #pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:9999 --outputEVNTFile %OUT.EVNT.root --jobConfig=100002 --maxEvents=1000" --outDS user.dcamarer.run2_100002_260319 --split 10 --maxCpuCount 259200
-pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:9998 --outputEVNTFile %OUT.EVNT.root --jobConfig=100002 --maxEvents=250" --outDS user.dcamarer.run2_100002_260321 --split 10 --maxCpuCount 259200
+pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:9998 --outputEVNTFile %OUT.EVNT.root --jobConfig=100002 --maxEvents=250" --outDS user.dcamarer.run2_100002_260321_bis --split 10 --maxCpuCount 259200
 
 echo ""
 date
