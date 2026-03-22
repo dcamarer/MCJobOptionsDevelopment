ee328a023db278a0137a150c1d87deab9343f2cd
dcamarer <daniel.camarero.munoz@cern.ch>
2026-03-22 15:23:52 +0100

    Another extension for SNLO 2.2.26

---
+++ JOs_yjet_SherpaNLO2216_DCMVal/13TeV/gen_900003_part5.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+asetup AthGeneration,23.6.49,here
+
+#WORKDIR="/afs/cern.ch/work/d/dcamarer/private/PostDoc/PMG/MCJobOptionsDevelopment/JOs_yjet_SherpaNLO2216_DCMVal/13TeV/900003_gen_part5"
+WORKDIR="/usatlas/u/dcamarero/MCJobOptionsDevelopment/JOs_yjet_SherpaNLO2216_DCMVal/13TeV/900003_gen_part5"
+
+# Check if workdir exists, abort if not
+if [ ! -d "$WORKDIR" ]; then
+    echo "ERROR: Workdir does not exist: $WORKDIR" >&2
+    echo "Aborting execution." >&2
+    exit 1
+fi
+
+cd "$WORKDIR" || exit 1
+
+Gen_tf.py --ecmEnergy=13000.0 \
+          --randomSeed=3141 \
+          --jobConfig=../900003 \
+          --outputEVNTFile=run2_900003_part5.EVNT.root \
+          --maxEvents=5000
