3dfcfa99ada5364278d226acb416543dce1b3480
dcamarer <daniel.camarero.munoz@cern.ch>
2026-03-25 20:18:24 +0100

    Adding --extFile option as default (II).

--- JOs_yjet_SherpaNLO2216_DCMVal/13TeV/900006_gen_GRID/gen_900006_GRID.sh
+++ JOs_yjet_SherpaNLO2216_DCMVal/13TeV/900006_gen_GRID/gen_900006_GRID.sh
@@ -2,7 +2,7 @@ echo ""
 date
 echo ""
 
-pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:9999 --outputEVNTFile %OUT.EVNT.root --jobConfig=900006 --maxEvents=4000" --outDS user.dcamarer.run2_900006_260320 --split 10 --maxCpuCount 259200
+pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:9999 --outputEVNTFile %OUT.EVNT.root --jobConfig=900006 --maxEvents=4000" --extFile 900006/mc_13TeV.Sh_2216_NNPDF30NNLO_SinglePhoton_pty_500_1000.GRID.tar.gz --outDS user.dcamarer.run2_900006_260325_v2 --split 10 --maxCpuCount 259200
 
 echo ""
 date
--- JOs_yjet_SherpaNLO2216_DCMVal/13TeV/900007_gen_GRID/gen_900007_GRID.sh
+++ JOs_yjet_SherpaNLO2216_DCMVal/13TeV/900007_gen_GRID/gen_900007_GRID.sh
@@ -2,7 +2,7 @@ echo ""
 date
 echo ""
 
-pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:9999 --outputEVNTFile %OUT.EVNT.root --jobConfig=900007 --maxEvents=800" --outDS user.dcamarer.run2_900007_260320 --split 10 --maxCpuCount 259200
+pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:9999 --outputEVNTFile %OUT.EVNT.root --jobConfig=900007 --maxEvents=2000" --extFile 900007/mc_13TeV.Sh_2216_NNPDF30NNLO_SinglePhoton_pty_1000_2000.GRID.tar.gz --outDS user.dcamarer.run2_900007_260325_v2 --split 10 --maxCpuCount 259200
 
 echo ""
 date
