11d6da88e6641f83d82adff1df8e04605dd46af3
dcamarer <daniel.camarero.munoz@cern.ch>
2026-04-24 14:24:45 +0200

    Preparing final plots for the same-sign WW analysis (II).

--- RIVET/setup.sh
+++ RIVET/setup.sh
@@ -3,16 +3,17 @@ date
 echo ""
 
 setupATLAS
+#setupATLAS -c centos7
 lsetup asetup
 
 ### Same-sign WW polarisation studies
-asetup AthGeneration,23.6.28,here
+#asetup AthGeneration,23.6.28,here
 ### Sherpa 2.2.16
 #asetup AthGeneration,23.6.49,here
 ### Sherpa 3.0.3
 #asetup AthGeneration,23.6.49,here
 ### Latest
-#asetup main,latest,AthGeneration
+asetup main,latest,AthGeneration
 
 ### Setup Rivet
 # Rivet 4 is not backwards compatible with Rivet 3 and crucially the DressedLeptons projection was removed https://gitlab.com/hepcedar/rivet/-/blob/release-4-1-x/doc/tutorials/mig3to4.md
