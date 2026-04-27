cbbf7c60a1f9e4615042f54b370e7905750cdae0
dcamarer <daniel.camarero.munoz@cern.ch>
2026-04-27 15:30:28 +0200

    Updating the setup script.

--- RIVET/setup.sh
+++ RIVET/setup.sh
@@ -1,22 +1,40 @@
+#!/bin/bash
+
 echo ""
 date
 echo ""
 
-setupATLAS
-#setupATLAS -c centos7
-lsetup asetup
+MODE="${1:-el9}"
 
-### Same-sign WW polarisation studies
-#asetup AthGeneration,23.6.28,here
-### Sherpa 2.2.16
-#asetup AthGeneration,23.6.49,here
-### Sherpa 3.0.3
-#asetup AthGeneration,23.6.49,here
-### Latest
-asetup main,latest,AthGeneration
+case "$MODE" in
+  c7)
+    setupATLAS -c centos7
+    lsetup asetup
+
+    ### Same-sign WW polarisation studies
+    asetup AthGeneration,23.6.28,here
+    ;;
+  el9)
+    setupATLAS
+    lsetup asetup
+
+    ### Sherpa 2.2.16
+    #asetup AthGeneration,23.6.49,here
+    ### Sherpa 3.0.3
+    #asetup AthGeneration,23.6.49,here
+    ### Latest
+    asetup main,latest,AthGeneration
+    ;;
+  *)
+    echo "ERROR: unknown mode '$MODE'"
+    echo "Usage: $0 [c7|el9]"
+    exit 1
+    ;;
+esac
 
 ### Setup Rivet
-# Rivet 4 is not backwards compatible with Rivet 3 and crucially the DressedLeptons projection was removed https://gitlab.com/hepcedar/rivet/-/blob/release-4-1-x/doc/tutorials/mig3to4.md
+# Rivet 4 is not backwards compatible with Rivet 3 and crucially the DressedLeptons projection was removed
+# https://gitlab.com/hepcedar/rivet/-/blob/release-4-1-x/doc/tutorials/mig3to4.md
 source setupRivet
 rivet --version
 
