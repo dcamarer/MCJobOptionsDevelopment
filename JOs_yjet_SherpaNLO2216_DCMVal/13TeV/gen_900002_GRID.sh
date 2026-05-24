1e3a18d0be79fc256a4f3fab1bd758523fefa773
dcamarer <daniel.camarero.munoz@cern.ch>
2026-05-24 19:04:54 +0200

    Minor updates to the 5. and GRID scripts.

--- JOs_yjet_SherpaNLO2216_DCMVal/13TeV/gen_900002_GRID.sh
+++ JOs_yjet_SherpaNLO2216_DCMVal/13TeV/gen_900002_GRID.sh
@@ -2,7 +2,7 @@ echo ""
 date
 echo ""
 
-pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:7777 --outputEVNTFile %OUT.EVNT.root --jobConfig=900002_ui --maxEvents=5000" --extFile 900002_ui/mc_13TeV.Sh_2216_NNPDF30NNLO_SinglePhoton_pty_35_70.GRID.tar.gz --outDS user.dcamarer.run2_900002_260524 --split 20 --maxCpuCount 259200
+pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:7777 --outputEVNTFile %OUT.EVNT.root --jobConfig=900002_ui --maxEvents=5000" --extFile 900002_ui/mc_13TeV.Sh_2216_NNPDF30NNLO_SinglePhoton_pty_35_70.GRID.tar.gz --outDS user.dcamarer.run2_900002_ui_260524 --split 20 --maxCpuCount 259200
 
 echo ""
 date
--- JOs_yjet_SherpaNLO2216_DCMVal/13TeV/gen_900005_GRID.sh
+++ JOs_yjet_SherpaNLO2216_DCMVal/13TeV/gen_900005_GRID.sh
@@ -2,7 +2,7 @@ echo ""
 date
 echo ""
 
-pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:7777 --outputEVNTFile %OUT.EVNT.root --jobConfig=900005_ui --maxEvents=1000" --extFile 900005_ui/mc_13TeV.Sh_2216_NNPDF30NNLO_SinglePhoton_pty_280_500.GRID.tar.gz --outDS user.dcamarer.run2_900005_260524 --split 20 --maxCpuCount 259200
+pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:7777 --outputEVNTFile %OUT.EVNT.root --jobConfig=900005_ui --maxEvents=1000" --extFile 900005_ui/mc_13TeV.Sh_2216_NNPDF30NNLO_SinglePhoton_pty_280_500.GRID.tar.gz --outDS user.dcamarer.run2_900005_ui_260524 --split 20 --maxCpuCount 259200
 
 echo ""
 date
