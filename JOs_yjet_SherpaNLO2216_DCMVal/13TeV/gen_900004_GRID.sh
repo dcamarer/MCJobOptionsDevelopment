0ecb318652af6bbf62651e0e884c3d3ca3df98e3
dcamarer <daniel.camarero.munoz@cern.ch>
2026-05-24 18:26:03 +0200

    Updating all GRID submission scripts (II).

--- JOs_yjet_SherpaNLO2216_DCMVal/13TeV/gen_900004_GRID.sh
+++ JOs_yjet_SherpaNLO2216_DCMVal/13TeV/gen_900004_GRID.sh
@@ -2,7 +2,7 @@ echo ""
 date
 echo ""
 
-pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:9999 --outputEVNTFile %OUT.EVNT.root --jobConfig=900004_ui --maxEvents=2000" --extFile 900004_ui/mc_13TeV.Sh_2216_NNPDF30NNLO_SinglePhoton_pty_140_280.GRID.tar.gz --outDS user.dcamarer.run2_900004_260524 --split 20 --maxCpuCount 259200
+pathena --trf "Gen_tf.py --ecmEnergy=13000 --randomSeed %RNDM:7777 --outputEVNTFile %OUT.EVNT.root --jobConfig=900004_ui --maxEvents=2000" --extFile 900004_ui/mc_13TeV.Sh_2216_NNPDF30NNLO_SinglePhoton_pty_140_280.GRID.tar.gz --outDS user.dcamarer.run2_900004_260524 --split 20 --maxCpuCount 259200
 
 echo ""
 date
