a7b71e6e4bfaa2cc69d9c0117b59b338596c8c5b
dcamarer <daniel.camarero.munoz@cern.ch>
2026-07-11 19:44:33 +0200

    Increasing the integration error.

--- JOs_yjet_SherpaNLO2216_DCMVal_final/13TeV/900005_ui/ecm13TeV/3.integrate.sh
+++ JOs_yjet_SherpaNLO2216_DCMVal_final/13TeV/900005_ui/ecm13TeV/3.integrate.sh
@@ -68,11 +68,11 @@ cat > Run.dat <<EOL
   RS_ME_Generator Comix {LJET};
   Loop_Generator LOOPGEN {LJET};
   PSI_ItMin 30000 {2,3};
-  Integration_Error 0.10 {2,3};
+  Integration_Error 0.25 {2,3};
   PSI_ItMin 50000 {4};
-  Integration_Error 0.10 {4};
+  Integration_Error 0.25 {4};
   PSI_ItMin 100000 {5};
-  Integration_Error 0.10 {5};
+  Integration_Error 0.25 {5};
   End process;
 }(processes)
 
