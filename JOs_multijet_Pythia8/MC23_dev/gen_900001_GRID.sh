b897787724ea936a041baed66122facae537875b
dcamarer <daniel.camarero.munoz@cern.ch>
2026-09-09 14:08:43 +0200

    Preparing the Pythia8 generation (IV).

--- JOs_multijet_Pythia8/MC23_dev/gen_900001_GRID.sh
+++ JOs_multijet_Pythia8/MC23_dev/gen_900001_GRID.sh
@@ -2,10 +2,12 @@ echo ""
 date
 echo ""
 
-# Production of 15000 * 10 = 150000 events
+# Production of 10000 * 10 = 100000 events
 # Part 1: RNDM:9999
+# Production of 20000 * 10 = 200000 events
+# Part 2: RNDM:8888
 
-pathena --trf "Gen_tf.py --ecmEnergy=13600 --randomSeed %RNDM:9999 --outputEVNTFile %OUT.EVNT.root --jobConfig=801166 --maxEvents=10000" --outDS user.dcamarer.run3_801166_ui_090926 --split 10 --maxCpuCount 259200
+pathena --trf "Gen_tf.py --ecmEnergy=13600 --randomSeed %RNDM:8888 --outputEVNTFile %OUT.EVNT.root --jobConfig=801166 --maxEvents=20000" --outDS user.dcamarer.run3_801166_ui_090926_ext1 --split 10 --maxCpuCount 259200
 
 echo ""
 date
